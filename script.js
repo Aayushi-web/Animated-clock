1a4ab490a850518174862570a6cf1040d4c75e54
Aayushi Mishra <mishraaayushi66@gmail.com>
2023-01-10 00:42:19 +0530

    feat: new featues

--- script.js
+++ script.js
@@ -9,4 +9,26 @@ let ss=day.getSeconds()*6;
 hr.style.transform=`rotateZ(${hh+(mm/12)}deg)`
 sc.style.transform=`rotateZ(${ss}deg)`
 mn.style.transform=`rotateZ(${mm}deg)`
+//digital clock
+
+let hours= document.getElementById('hour')
+let minutes= document.getElementById('minutes')
+let second= document.getElementById('seconds')
+
+let ampm= document.getElementById('ampm')
+let h= new Date().getHours();
+let m= new Date().getMinutes();
+let s= new Date().getSeconds();
+let am= h>=12 ? "pm":"am";
+//add zero brfore single digit 
+h= (h<10)?"0"+h:h;
+m= (m<10)?"0"+m:m;
+s= (s<10)?"0"+s:s;
+
+//inner html
+hours.innerHTML=h;
+minutes.innerHTML=m;
+second.innerHTML=s;
 })
+
+
